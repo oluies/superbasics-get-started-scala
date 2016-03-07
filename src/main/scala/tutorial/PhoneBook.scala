e1ad31173fd7948708fc786d5b3184de94907f9e
Jesper Öman <jo@magine.com>
2016-03-07 20:14:16 +0100

    More excercises

--- src/main/scala/tutorial/PhoneBook.scala
+++ src/main/scala/tutorial/PhoneBook.scala
@@ -3,12 +3,80 @@ package tutorial
 case class Contact(name: String, phone: String, address: String)
 
 case class PhoneBook(contacts: List[Contact], name: String) {
+
+  /**
+    * Excercise 1
+    * Return a new PhoneBook including the new contact
+    * @param contact
+    * @return
+    */
+  def +(contact: Contact): PhoneBook = ???
+
+  /**
+    * Excercise 2
+    * Return a list of contacts, sorted by name, a-z
+    * @return
+    */
+  def sortByName: List[Contact] = ???
+
+  /**
+    * Excercise 3
+    * Return a new PhoneBook excluding the contact(s) matching exactly the
+    * name supplied
+    * @param name
+    * @return
+    */
+  def -(name: String): PhoneBook = ???
+
+  /**
+    * Excercise 4
+    * Implement a function that is able to search for contacts by parts
+    * of their names
+    * @param name
+    * @return List of contacts
+    */
   def search(name: String): List[Contact] = ???
 
-  def byName: List[Contact] = ???
+  /**
+    * Excercise 5
+    * Create a new PhoneBook consisting of this and all records of "other"
+    * PhoneBook
+    * @param other
+    * @return
+    */
+  def ++(other: PhoneBook): PhoneBook = ???
 
-  def add(contact: Contact): PhoneBook = ???
+  /**
+    * Excercise 6
+    * Return the first Contact matching the name supplied
+    * @param name
+    * @return
+    */
+  def findByName(name: String): Contact = ???
 
-  def remove(name: String): PhoneBook = ???
+  /**
+    * Excercise 7
+    * Return the first Contact matching the number
+    * @param number
+    * @return
+    */
+  def findByNumber(number: String): Contact = ???
 
+  /**
+    * Excercise 8
+    * Return the number of the first contact whos name is matching supplied
+    * string
+    * @param name
+    * @return
+    */
+  def findNr(name: String): String = ???
+
+  /**
+    * Excercise 9
+    * Return the name of the first contact who's number is matching
+    * supplied string
+    * @param number
+    * @return
+    */
+  def findName(number: String): String = ???
 }
--- src/test/scala/tutorial/PhoneBookSpec.scala
+++ src/test/scala/tutorial/PhoneBookSpec.scala
@@ -4,33 +4,63 @@ import org.scalatest.{Matchers, WordSpec}
 
 class PhoneBookSpec extends WordSpec with Matchers {
   "tutorial.PhoneBook" should {
-    "add contact" in {
+    "Excercise 1: add contact" in {
       emptyBook.contacts shouldBe List()
       val dude = Contact("Some dude", "", "")
-      val withNisse = emptyBook.add(dude)
+      val withNisse = emptyBook + dude
       withNisse.contacts shouldBe List(dude)
     }
 
-    "Sort by name" in {
+    "Excercise 2: Sort by name" in {
       fullBook.contacts.head.name shouldBe "Chip Munk"
-      val sorted = fullBook.byName
+      val sorted = fullBook.sortByName
       sorted.head.name shouldBe "Anna Conda"
     }
 
-    "Remove contact" in {
-      val noNisse = fullBook.remove("Ferris Wheeler")
+    "Excercise 3: Remove contact" in {
+      val noNisse = fullBook - "Ferris Wheeler"
       noNisse.contacts shouldNot contain(Contact("Ferris Wheeler", "08123456", "1 Main street"))
     }
 
-    "Search contacts" in {
+    "Excercise 4: Search contacts" in {
       fullBook.search("Armand") shouldBe List(Contact("Harry Armand Bach", "08123456", "1 Main street"))
     }
 
-    "get multiple contacts in search result" in {
+    "Excercise 4: get multiple contacts in search result" in {
       fullBook.search("Anna") shouldBe List(
         Contact("Anna Conda", "08123456", "1 Main street"),
         Contact("Anna Fender", "08123456", "1 Main street")
       )
+    }
+
+    "Excercise 5: concat 2 phonebooks" in {
+      val expected = List(
+        Contact("Chip Munk", "08123456", "1 Main street"),
+        Contact("Earl E. Bird", "08123456", "1 Main street"),
+        Contact("Ferris Wheeler", "08123456", "1 Main street"),
+        Contact("Harry Armand Bach", "087342342", "1 Main street"),
+        Contact("Anna Conda", "08123456", "1 Main street"),
+        Contact("Anna Fender", "08123456", "1 Main street"),
+        Contact("Brad Hammer ", "08123456", "1 Main street"),
+        Contact("Morey Bund", "555-1223", "45 Hudson"),
+        Contact("Ray Gunn", "555-1223", "45 Hudson"),
+        Contact("Russell Sprout", "555-1223", "45 Hudson"),
+        Contact("Will Power", "555-1223", "45 Hudson"),
+        Contact("Iona Ford", "555-1223", "45 Hudson")
+      )
+      (fullBook ++ otherBook).contacts shouldBe expected
+    }
+    "Excercise 6: Return the first matching contact" in {
+      fullBook.findByName("Anna Conda") shouldBe Contact("Anna Conda", "08123456", "1 Main street")
+    }
+    "Excercise 7: Return first matching by number" in {
+      fullBook.findByNumber("087342342") shouldBe Contact("Harry Armand Bach", "087342342", "1 Main street")
+    }
+    "Excercise 8: " in {
+      otherBook.findNr("Will Power") shouldBe "555-1223"
+    }
+    "Excercise 9: " in {
+      fullBook.findName("087342342") shouldBe "Harry Armand Bach"
     }
   }
 
@@ -39,11 +69,19 @@ class PhoneBookSpec extends WordSpec with Matchers {
     Contact("Chip Munk", "08123456", "1 Main street"),
     Contact("Earl E. Bird", "08123456", "1 Main street"),
     Contact("Ferris Wheeler", "08123456", "1 Main street"),
-    Contact("Harry Armand Bach", "08123456", "1 Main street"),
+    Contact("Harry Armand Bach", "087342342", "1 Main street"),
     Contact("Anna Conda", "08123456", "1 Main street"),
     Contact("Anna Fender", "08123456", "1 Main street"),
     Contact("Brad Hammer ", "08123456", "1 Main street")
   )
   lazy val fullBook = PhoneBook(contacts, "Full")
+  lazy val other = List(
+    Contact("Morey Bund", "555-1223", "45 Hudson"),
+    Contact("Ray Gunn", "555-1223", "45 Hudson"),
+    Contact("Russell Sprout", "555-1223", "45 Hudson"),
+    Contact("Will Power", "555-1223", "45 Hudson"),
+    Contact("Iona Ford", "555-1223", "45 Hudson")
+  )
+  lazy val otherBook = PhoneBook(other, "Other")
 }
 
